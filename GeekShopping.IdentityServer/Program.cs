833521e6959f19a42bf96ab4f6fc1fd2c24ff01a
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-09-05 19:41:48 -0300

    Enable Https and Setting Ports

--- GeekShopping.IdentityServer/Program.cs
+++ GeekShopping.IdentityServer/Program.cs
@@ -1,7 +1,37 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MySQLContext>(options =>
+options.UseMySql(
+    builder.Configuration.GetConnectionString("MySQLConnection"),
+    new MySqlServerVersion(new Version(8, 0, 30))));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySQLContext>()
+    .AddDefaultTokenProviders();
+
+ IIdentityServerBuilder builderServices = builder.Services.AddIdentityServer(options =>
+ {
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+ })
+ .AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+ .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+ .AddInMemoryClients(IdentityConfiguration.Clients)
+ .AddAspNetIdentity<ApplicationUser>();
+
+builderServices.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
@@ -10,9 +40,16 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+app.UseHttpsRedirection();
+
+app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
