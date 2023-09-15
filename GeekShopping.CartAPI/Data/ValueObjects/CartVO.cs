6b673a81c9717b976ddd7306f3aae9033dd3d892
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-09-15 19:25:48 -0300

    Creating ValueObjects and Setting AutoMapper Configurations

---
+++ GeekShopping.CartAPI/Data/ValueObjects/CartVO.cs
@@ -0,0 +1,8 @@
+﻿namespace GeekShopping.CartAPI.Data.ValueObjects
+{
+    public class CartVO
+    {
+        public CartHeaderVO CartHeader { get; set; }
+        IEnumerable<CartDetailVO> CartDetails { get; set; }
+    }
+}
