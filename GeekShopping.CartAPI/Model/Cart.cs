4f1fde365595cea9aa3f5b4545c947089d937c3e
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-09-15 19:17:02 -0300

    Setting DbContext and Applying Migrations

---
+++ GeekShopping.CartAPI/Model/Cart.cs
@@ -0,0 +1,8 @@
+﻿namespace GeekShopping.CartAPI.Model
+{
+    public class Cart
+    {
+        public CartHeader CartHeader { get; set; }
+        IEnumerable<CartDetail> CartDetails { get; set; }
+    }
+}
