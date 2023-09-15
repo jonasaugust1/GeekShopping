cc9ddc5baf342146c79b0aba7ae151479a72c4fd
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-09-15 19:31:20 -0300

    Implementing CartRepository

---
+++ GeekShopping.CartAPI/Repository/CardRepository.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Model.Context;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public class CardRepository : ICartRepository
+    {
+        private readonly MySQLContext _context;
+        private IMapper _mapper;
+
+        public CardRepository(MySQLContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public Task<bool> ApplyCoupon(string userID, string couponCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ClearCart(string userID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartVO> FindCartByUserID(string userID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveCoupon(string userID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromCart(long cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartVO> SaveOrUpdateCart(CartVO cart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GeekShopping.CartAPI/Repository/ICartRepository.cs
@@ -0,0 +1,16 @@
+﻿using GeekShopping.CartAPI.Data.ValueObjects;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public interface ICartRepository
+    {
+        Task<CartVO> FindCartByUserID(string userID);
+        Task<CartVO> SaveOrUpdateCart(CartVO cart);
+        Task<bool> RemoveFromCart(long cartDetailsId);
+        Task<bool> ApplyCoupon(string userID, string couponCode);
+        Task<bool> RemoveCoupon(string userID);
+        Task<bool> ClearCart(string userID);
+
+
+    }
+}
