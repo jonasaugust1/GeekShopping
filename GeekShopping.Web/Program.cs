79b2162dd9b1cd6ca3c7f6bced2e8bcd39b5c434
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-09-21 22:50:44 -0300

    Consuming Coupon Service

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -38,10 +38,10 @@ builder.Services.AddHttpClient<ICartService, CartService>(c =>
     c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CartAPI"]);
 });
 
-//builder.Services.AddHttpClient<ICouponService, CouponService>(c =>
-//{
-//    c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"]);
-//});
+builder.Services.AddHttpClient<ICouponService, CouponService>(c =>
+{
+    c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:CouponAPI"]);
+});
 
 var app = builder.Build();
 
