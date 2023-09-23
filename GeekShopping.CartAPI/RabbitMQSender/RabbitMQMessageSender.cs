d0df6a6f3ec6b33a78e4469c404eec8a841a1733
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-09-23 14:11:31 -0300

    Adding RabbitMQ to the Project and Implementing RabbitMQMessageSender

---
+++ GeekShopping.CartAPI/RabbitMQSender/RabbitMQMessageSender.cs
@@ -0,0 +1,53 @@
+﻿using GeekShopping.MessageBus;
+using RabbitMQ.Client;
+
+namespace GeekShopping.CartAPI.RabbitMQSender
+{
+    public class RabbitMQMessageSender : IRabbitMQSender
+    {
+        private readonly string _hostName;
+        private readonly string _password;
+        private readonly string _userName;
+        private IConnection _connection;
+
+        public RabbitMQMessageSender()
+        {
+            _hostName = "localhost";
+            _password = "guest";
+            _userName = "guest";
+        }
+
+        public void SendMessage(BaseMessage baseMessage, string queueName)
+        {
+            ConnectionFactory factory = new()
+            {
+                HostName = _hostName,
+                UserName = _userName,
+                Password = _password,
+            };
+
+            _connection = factory.CreateConnection();
+
+            using IModel? channel = _connection.CreateModel();
+
+            if(channel != null)
+            {
+                channel.QueueDeclare(queueName, false, false, false, arguments: null);
+
+                byte[] body = GetMessageAsByteArray(baseMessage);
+
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: queueName,
+                    basicProperties: null,
+                    body: body
+                    );
+            }
+        }
+
+        private byte[] GetMessageAsByteArray(BaseMessage baseMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
