7f14a171d9022d6fc0e9fe934384729bfa1a2e78
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-09-23 15:31:53 -0300

    Initiating RabbitMQCheckoutConsumer

---
+++ GeekShopping.OrderAPI/MessageConsumer/RabbitMQMessageConsumer.cs
@@ -0,0 +1,36 @@
+﻿using GeekShopping.CartAPI.Repository;
+using GeekShopping.MessageBus;
+using RabbitMQ.Client;
+using System.Threading.Channels;
+
+namespace GeekShopping.OrderAPI.MessageConsumer
+{
+    public class RabbitMQMessageConsumer : BackgroundService
+    {
+        private readonly OrderRepository _repository;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public RabbitMQMessageConsumer(OrderRepository repository)
+        {
+            _repository = repository;
+
+            ConnectionFactory factory = new()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+            };
+
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            _channel.QueueDeclare("checkout_queue", false, false, false, arguments: null);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
