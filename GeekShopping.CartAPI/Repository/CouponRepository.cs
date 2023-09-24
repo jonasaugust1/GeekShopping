0b2dbd5c900f7cdf5cbf35ec4641e759484d4787
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-09-23 22:05:50 -0300

    Implementing Coupon Repository Classes in CartAPI

---
+++ GeekShopping.CartAPI/Repository/CouponRepository.cs
@@ -0,0 +1,29 @@
+﻿using GeekShopping.CartAPI.Data.ValueObjects;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public class CouponRepository : ICouponRepository
+    {
+        private readonly HttpClient _client;
+
+        public async Task<CouponVO> GetCouponByCouponCode(string couponCode, string token)
+        {
+            
+
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            
+            HttpResponseMessage response = await _client.GetAsync($"api/v1/coupon/{couponCode}");
+
+            string? content = await response.Content.ReadAsStringAsync();
+
+            if(response.StatusCode != HttpStatusCode.OK) return new CouponVO();
+
+            return JsonSerializer.Deserialize<CouponVO>(
+                content,
+                options: new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
