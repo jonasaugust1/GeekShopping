161e528253f782d42357ee39ca15c9c437a5fea9
Jonas Augusto <95719426+jonasaugust1@users.noreply.github.com>
2023-09-27 17:51:18 -0300

    Implementing Messages, Entities and Context

---
+++ GeekShopping.Email/Repository/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using GeekShopping.Email.Model;
+
+namespace GeekShopping.Email.Repository
+{
+    public interface IOrderRepository
+    {
+        Task UpdateOrderStatus(long orderHeaderId, bool paid);
+    }
+}
